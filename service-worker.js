d39f7dfb1735608163443b79b2547f5087d93b0b
Redrogue12 <exgc12@gmail.com>
2019-07-21 01:30:39 -0400

    init

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('Fourfold')
+      .then(function (cache) {
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/style.css',
+          '/index.js'
+        ])
+      })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (res) {
+        return res;
+      })
+  );
+});
