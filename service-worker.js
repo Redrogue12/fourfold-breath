f4edb73db613cf21bb0de387a00afa80bc588c33
Redrogue12 <exgc12@gmail.com>
2019-07-22 12:42:16 -0400

    temp commented out

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open('Fourfold')
-      .then(function (cache) {
-        cache.addAll([
-          '/',
-          '/index.html',
-          '/style.css',
-          '/index.js'
-        ])
-      })
-  );
-  return self.clients.claim();
-});
+// self.addEventListener('install', function (event) {
+//   event.waitUntil(
+//     caches.open('Fourfold')
+//       .then(function (cache) {
+//         cache.addAll([
+//           '/',
+//           '/index.html',
+//           '/style.css',
+//           '/index.js'
+//         ])
+//       })
+//   );
+//   return self.clients.claim();
+// });
 
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function (res) {
-        return res;
-      })
-  );
-});
+// self.addEventListener('fetch', function (event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function (res) {
+//         return res;
+//       })
+//   );
+// });
